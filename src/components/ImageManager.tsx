6615653f74ae210165183fcbbabfee484e14cb36
Kitze <kristijan.mkd@gmail.com>
2024-05-10 14:09:09 +0200

    separate image manager image

--- src/components/ImageManager.tsx
+++ src/components/ImageManager.tsx
@@ -2,6 +2,8 @@ import { UseArray } from "react-hanger";
 import { Image } from "@/types.ts";
 import React from "react";
 import { Button, Input } from "@/components/base.tsx";
+import { Images } from "@/components/Images.tsx";
+import { ImageManagerImage } from "@/components/ImageManagerImage.tsx";
 
 export const ImageManager = ({
   images,
@@ -39,56 +41,7 @@ export const ImageManager = ({
 
         <div className="vertical center space-y-2">
           {images.value.map((i) => (
-            <div className="horizontal center space-x-2" key={i.id}>
-              <Input
-                value={i.url}
-                onChange={(e) => {
-                  const newImages = images.value.map((im) =>
-                    im.id === i.id
-                      ? {
-                          ...im,
-                          url: e.target.value,
-                        }
-                      : im,
-                  );
-                  images.setValue(newImages);
-                }}
-              />
-              <label
-                htmlFor="file-upload"
-                className="cursor-pointer text-xl bg-transparent hover:bg-gray-100 p-2 rounded"
-                title="Upload image"
-              >
-                🖼️
-              </label>
-              <input
-                id="file-upload"
-                type="file"
-                className="hidden"
-                onChange={(e) => {
-                  const file = e.target.files[0];
-                  if (file) {
-                    const reader = new FileReader();
-                    reader.onload = (e) => {
-                      const newImages = images.value.map((im) =>
-                        im.id === i.id
-                          ? { ...im, url: e.target.result.toString() }
-                          : im,
-                      );
-                      images.setValue(newImages);
-                    };
-                    reader.readAsDataURL(file);
-                  }
-                }}
-              />
-
-              <button
-                className="text-xs"
-                onClick={() => images.removeById(i.id)}
-              >
-                🗑️
-              </button>
-            </div>
+            <ImageManagerImage key={i.id} images={images} i={i} />
           ))}
         </div>
       </div>
